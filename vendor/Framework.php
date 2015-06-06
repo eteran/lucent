8bd4eb0e50901b770e59bdbbd3ce79a45c31f234
Evan Teran <evan.teran@gmail.com>
2015-06-06 17:21:04 -0400

    enabling Input code

--- vendor/Framework.php
+++ vendor/Framework.php
@@ -4,6 +4,9 @@ require_once('Request.php');
 require_once('Response.php');
 require_once('View.php');
 require_once('Route.php');
+require_once('Input.php');
+
+// TODO(eteran): implement some clever autoloading
 
 require_once(__DIR__.'/../app/config/config.php');
 require_once(__DIR__.'/../app/config/routes.php');
